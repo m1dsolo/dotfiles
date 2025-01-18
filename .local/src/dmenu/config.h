ae9ad400ba2be311f4e7850d67618f3731063706
m1dsolo <yx1053532442@gmail.com>
2025-01-18 13:21:39 +0800

    feat: add dmenu `center` patch

--- .local/src/dmenu/config.h
+++ .local/src/dmenu/config.h
@@ -2,9 +2,12 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int centered = 1;                    /* -c option; centers dmenu on screen */
+static int min_width = 500;                    /* minimum width when centered */
+static const float menu_height_ratio = 4.0f;  /* This is the ratio used in the original calculation */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Source Code Pro:pixelsize=16:antialias=true:autohint=true"
+	"Source Code Pro:pixelsize=20:antialias=true:autohint=true"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
