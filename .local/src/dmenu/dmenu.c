47e9eaa5a8a2e763f63263510f2bc3fca0132e94
m1dsolo <yx1053532442@gmail.com>
2026-02-23 11:52:12 +0800

    feat: dmenu remap Ctrl+jk for item navigation

--- .local/src/dmenu/dmenu.c
+++ .local/src/dmenu/dmenu.c
@@ -359,17 +359,19 @@ keypress(XKeyEvent *ev)
 		case XK_g: ksym = XK_Escape;    break;
 		case XK_h: ksym = XK_BackSpace; break;
 		case XK_i: ksym = XK_Tab;       break;
-		case XK_j: /* fallthrough */
-		case XK_J: /* fallthrough */
+		case XK_j:
+		case XK_J:
+			ksym = XK_Tab;
+			break;
+		case XK_k:
+		case XK_K:
+			ksym = XK_ISO_Left_Tab;
+			break;
 		case XK_m: /* fallthrough */
 		case XK_M: ksym = XK_Return; ev->state &= ~ControlMask; break;
 		case XK_n: ksym = XK_Down;      break;
 		case XK_p: ksym = XK_Up;        break;
 
-		case XK_k: /* delete right */
-			text[cursor] = '\0';
-			match();
-			break;
 		case XK_u: /* delete left */
 			insert(NULL, 0 - cursor);
 			break;
