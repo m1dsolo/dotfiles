dabb0f9b53804cc725b1fc585eda39bff617eefc
m1dsolo <yx1053532442@gmail.com>
2026-02-25 23:38:29 +0800

    feat(dmenu): Ctrl+v to paste from clipboard
    Add Ctrl+v documentation for paste from clipboard

--- .local/src/dmenu/dmenu.c
+++ .local/src/dmenu/dmenu.c
@@ -387,6 +387,9 @@ keypress(XKeyEvent *ev)
 			while (cursor > 0 && !strchr(worddelimiters, text[nextrune(-1)]))
 				insert(NULL, nextrune(-1) - cursor);
 			break;
+		case XK_v:
+			XConvertSelection(dpy, clip, utf8, utf8, win, CurrentTime);
+			return;
 		case XK_y: /* paste selection */
 		case XK_Y:
 			XConvertSelection(dpy, (ev->state & ShiftMask) ? clip : XA_PRIMARY,
